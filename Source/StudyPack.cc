41b11adef0ae0a26882cce03d7c53e029b56b981
Ben Morgan <bmorgan.warwick@gmail.com>
2012-05-15 15:16:34 +0100

    Implemented basic layout and build for StudyPack.
    Simply build the executable, and use CPack out the box.

---
+++ Source/StudyPack.cc
@@ -0,0 +1,21 @@
+// StudyPack - hello world!
+//
+//---------------------------------------------------------------------------
+// Copyright (c) 2012 by Ben Morgan <bmorgan.warwick@gmail.com> 
+// Copyright (c) 2012 by The University of Warwick
+//
+// Distributed under the OSI-approved 3-Clause License (the "License");
+// see the accompanying file "License.txt" for details.
+//
+// This software is distributed WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the License for more information.
+//---------------------------------------------------------------------------
+
+#include <iostream>
+
+int main() {
+  std::cout << "[StudyPack::Info] Hello World" << std::endl;
+  return 0;
+}
+
