67461d91ea566e5be7b4f89da911e7755f0d8f2f
Ben Morgan <bmorgan.warwick@gmail.com>
2012-05-18 14:50:18 +0100

    Clarify license as BSD 3-Clause.

--- Source/StudyPack.cc
+++ Source/StudyPack.cc
@@ -1,16 +1,16 @@
 // StudyPack - hello world!
 //
-//---------------------------------------------------------------------------
+//----------------------------------------------------------------------
 // Copyright (c) 2012 by Ben Morgan <bmorgan.warwick@gmail.com> 
 // Copyright (c) 2012 by The University of Warwick
 //
-// Distributed under the OSI-approved 3-Clause License (the "License");
+// Distributed under the OSI-approved BSD 3-Clause License (the "License");
 // see the accompanying file "License.txt" for details.
 //
 // This software is distributed WITHOUT ANY WARRANTY; without even the
 // implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the License for more information.
-//---------------------------------------------------------------------------
+//----------------------------------------------------------------------
 
 #include <iostream>
 
